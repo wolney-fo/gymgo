0f851927bbf1e941b709fe44da63c2c2d7a6d5e8
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2024-04-21 16:10:30 -0300

    Aula: Handler de erros global

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,24 @@
+import { ZodError } from "zod";
 import { appRoutes } from "./http/routes";
 import fastify from "fastify";
+import { env } from "./env";
 
 export const app = fastify();
 
 app.register(appRoutes);
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: "Validation error.", issues: error.format() });
+  }
+
+  if (env.NODE_ENV !== "production") {
+    console.error(error);
+  } else {
+    // TODO: Create a log to external tool. (DataDog/NewRelic/Sentry)
+  }
+
+  return reply.status(500).send({ message: "Internal server error." });
+});
