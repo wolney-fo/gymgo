a669161b2762c8b14bb1812f8a31eb1e9fc536a7
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2024-04-28 16:56:04 -0300

    Aula: Primeiro teste unitário

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,5 +1,36 @@
-import { expect, it } from "vitest";
+import { expect, it, describe } from "vitest";
+import { RegisterUseCase } from "./register";
+import { compare } from "bcryptjs";
 
-it("should work", () => {
-  expect(2 + 2).toBe(4);
+describe("Register Use Case", () => {
+  it("should hash user password at registration time", async () => {
+    const registerUseCase = new RegisterUseCase({
+      async findByEmail(email) {
+        return null;
+      },
+
+      async create(data) {
+        return {
+          id: "user-1",
+          name: data.name,
+          email: data.email,
+          password_hash: data.password_hash,
+          created_at: new Date(),
+        };
+      },
+    });
+
+    const { user } = await registerUseCase.execute({
+      name: "John Doe",
+      email: "johndoe@example.com",
+      password: "123456",
+    });
+
+    const isPasswordCorrectlyHashed = await compare(
+      "123456",
+      user.password_hash
+    );
+
+    expect(isPasswordCorrectlyHashed).toBe(true);
+  });
 });
