14e49159d542580670c39b30ffa2d1555df23fac
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2024-04-28 17:17:39 -0300

    Ajustando nome do arquivo

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,4 +1,4 @@
-import { InMemoryUsersRepository } from "../repositories/in-memory/in-memory-users-respository";
+import { InMemoryUsersRepository } from "../repositories/in-memory/in-memory-users-repository";
 import { UserAlreadyExistsError } from "./errors/user-already-exists-error";
 import { expect, it, describe } from "vitest";
 import { RegisterUseCase } from "./register";
