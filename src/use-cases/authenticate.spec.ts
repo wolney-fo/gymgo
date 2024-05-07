716a5b89bf8f45f26522b0417a4bcff3f8742254
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2024-05-06 21:24:50 -0300

    Aula: Refatorando instâncias nos testes

--- src/use-cases/authenticate.spec.ts
+++ src/use-cases/authenticate.spec.ts
@@ -1,8 +1,8 @@
 import { InMemoryUsersRepository } from "../repositories/in-memory/in-memory-users-repository";
+import { InvalidCredentialsError } from "./errors/invalid-credentials-error";
 import { beforeEach, describe, expect, it } from "vitest";
 import { AuthenticateUseCase } from "./authenticate";
 import { hash } from "bcryptjs";
-import { InvalidCredentialsError } from "./errors/invalid-credentials-error";
 
 let usersRepository: InMemoryUsersRepository;
 let sut: AuthenticateUseCase;
