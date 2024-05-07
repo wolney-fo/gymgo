8a2bb81766e37dc916857a8a08a270c78160db3b
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2024-05-06 21:33:30 -0300

    Aula: Utilizando Factory Pattern

--- src/http/controllers/authenticate.controller.ts
+++ src/http/controllers/authenticate.controller.ts
@@ -1,8 +1,7 @@
 import { z } from "zod";
 import { FastifyReply, FastifyRequest } from "fastify";
-import { PrismaUsersRepository } from "../../repositories/prisma/prisma-users-repository";
 import { InvalidCredentialsError } from "../../use-cases/errors/invalid-credentials-error";
-import { AuthenticateUseCase } from "../../use-cases/authenticate";
+import { makeAuthenticateUseCase } from "../../use-cases/factories/make-authenticate-use-case";
 
 export async function authenticate(
   request: FastifyRequest,
@@ -16,8 +15,7 @@ export async function authenticate(
   const { email, password } = authenticateBodySchema.parse(request.body);
 
   try {
-    const userRepository = new PrismaUsersRepository();
-    const authenticateUseCase = new AuthenticateUseCase(userRepository);
+    const authenticateUseCase = makeAuthenticateUseCase();
 
     await authenticateUseCase.execute({ email, password });
   } catch (error) {
