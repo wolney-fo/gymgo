2d98f4cd9fe0e3bf2363de98bd10030727f7c023
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2024-05-17 13:19:22 -0300

    Aula: Implementando o JWT no Fastify

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,13 @@ import { ZodError } from "zod";
 import { appRoutes } from "./http/routes";
 import fastify from "fastify";
 import { env } from "./env";
+import fastifyJwt from "@fastify/jwt";
 
 export const app = fastify();
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+});
 
 app.register(appRoutes);
 
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,11 @@
 import { FastifyInstance } from "fastify";
 import { register } from "./controllers/register.controller";
 import { authenticate } from "./controllers/authenticate.controller";
+import { profile } from "./controllers/profile.controller";
 
 export async function appRoutes(app: FastifyInstance) {
   app.post("/users", register);
   app.post("/sessions", authenticate);
+
+  app.get("/me", profile)
 }
