0d5b0dc5f65fff41bb89e92eb3decb2653f1de58
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2024-06-16 15:19:43 -0300

    Aula: Controllers das rotas de check-ins

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import { ZodError } from "zod";
 import { usersRoutes } from "./http/controllers/users/routes";
 import { gymsRoutes } from "./http/controllers/gyms/routes";
+import { checkInsRoutes } from "./http/controllers/check-ins/routes";
 import fastify from "fastify";
 import { env } from "./env";
 import fastifyJwt from "@fastify/jwt";
@@ -13,6 +14,7 @@ app.register(fastifyJwt, {
 
 app.register(usersRoutes);
 app.register(gymsRoutes);
+app.register(checkInsRoutes);
 
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
