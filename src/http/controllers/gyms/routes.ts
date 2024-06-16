34b98b12d2141336b90f1e735c056f5c93045f1e
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2024-06-16 14:58:16 -0300

    Aula: Outros controllers da academia

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,6 +1,14 @@
 import { verifyJWT } from "../../middlewares/verify-jwt";
 import { FastifyInstance } from "fastify";
+import { search } from "./search.controller";
+import { nearby } from "./nearby.controller";
+import { create } from "./create.controller";
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook("onRequest", verifyJWT);
+
+  app.get("/gym/search", search);
+  app.get("/gym/nearby", nearby);
+
+  app.post("/gyms", create);
 }
