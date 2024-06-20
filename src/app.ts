2bd24e9038a715c04f052032e7f369b8ae751fae
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2024-06-19 22:34:38 -0300

    Aula: Estrategia de refresh de token

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,22 @@ import { checkInsRoutes } from "./http/controllers/check-ins/routes";
 import fastify from "fastify";
 import { env } from "./env";
 import fastifyJwt from "@fastify/jwt";
+import fastifyCookie from "@fastify/cookie";
 
 export const app = fastify();
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: "refreshToken",
+    signed: false,
+  },
+  sign: {
+    expiresIn: "10m",
+  },
 });
+
+app.register(fastifyCookie);
 
 app.register(usersRoutes);
 app.register(gymsRoutes);
